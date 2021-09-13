94140f374c237328ea3527cf05b1229144bd0952
이강찬 <lsc4237@gmail.com>
2021-09-13 18:39:33 +0900

    Material 사용

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import './App.css';
 import { Component } from 'react/cjs/react.production.min';
 import Customer from './components/Customer'
 
+import Papar from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles} from '@material-ui/core/styles'; // style불러오는 것
+import { classes } from 'istanbul-lib-coverage';
+
+const styles = theme => ({
+  root: {
+    width : '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 const customer = [
   {'id' : 1,
   'image' : 'https://placeimg.com/64/64/1',
@@ -32,26 +51,23 @@ class App extends Component{
 
   render(){
     return (
-      <div className="gray-background">
-        {
-          customer.map(c => {
-            return (
-            <Customer 
-              key={c.id}
-              id={c.id} 
-              img = {c.image} 
-              name={c.name} 
-              birthday = {c.birthday} 
-              gender = {c.gender} 
-              job = {c.job}
-            />
-            );
-          })
-        }
-        
-      </div>
+      <Papar className ={classes.root}>
+        <Table className = {classes.table}>
+          <TableHead>
+            <TableCell> 번호</TableCell>
+            <TableCell> 이미지</TableCell>
+            <TableCell> 이름</TableCell>
+            <TableCell> 생일</TableCell>
+            <TableCell> 젠더</TableCell>
+            <TableCell> 직업</TableCell>
+          </TableHead>
+          <TableBody>
+            { customer.map(c => { return ( <Customer key={c.id} id={c.id} img = {c.image} name={c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job}/> ); }) }
+          </TableBody>
+        </Table>
+      </Papar>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
