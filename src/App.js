bedafb4e41ffe86e0c29d649d66378e86b8f5334
이강찬 <lsc4237@gmail.com>
2021-09-13 18:10:21 +0900

    create react prectice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react/cjs/react.production.min';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer = [
+  {'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : "홍길동",
+  'birthday' : '960753',
+  'gender' : 'man',
+  'job' : 'job'
+  },
+  {'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : "길동이",
+  'birthday' : '960743',
+  'gender' : 'man',
+  'job' : '대학생'
+  },
+  {'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : "찬이",
+  'birthday' : '910753',
+  'gender' : 'woman',
+  'job' : '고등학생'
+  }
+
+]
+
+class App extends Component{
+
+  render(){
+    return (
+      <div className="gray-background">
+        {
+          customer.map(c => {
+            return (
+            <Customer 
+              key={c.id}
+              id={c.id} 
+              img = {c.image} 
+              name={c.name} 
+              birthday = {c.birthday} 
+              gender = {c.gender} 
+              job = {c.job}
+            />
+            );
+          })
+        }
+        
+      </div>
+    );
+  }
 }
 
 export default App;
